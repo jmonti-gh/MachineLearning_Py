afcacbc1d0213cd7a2b4ea34ae5a33b4ccaefd16
jm <jorgitomonti@gmail.com>
2023-04-12 17:09:11 -0300

    finish kaggle_ML_4

---
+++ draft_1.py
@@ -0,0 +1,26 @@
+#
+
+print(f'\nValidation data home price average (df.mean()): {round(1172325.967634, 2):,}\n')
+
+print(f'\nVal_df.mean(): {round(1172325.967634, 2):,}\n'.center(50, '-'))
+
+print('\nVal_df.mean(): {round(1172325.967634, 2):,}\n'.center(70, '-'))
+
+txt = "banana"
+x = txt.center(20)
+print(x)
+print(f'  banana {5622:,}  '.center(40, '-'))
+
+print()
+t2 = 'pineapple'
+print('-' * len(t2) + '\n' + t2 + '\n' + '-' * len(t2) )
+print()
+print('\npineapple\n'.center(len('_pineapple') * 3, '-'))
+print()
+print(f"{'-' * len(t2)}\n{t2}\n{'-' * len(t2)}")
+print()
+print('{0}\n{1}\n{0}'.format('-' * len(t2), t2))
+
+
+
+
